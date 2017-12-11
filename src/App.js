be235073b358624937d36e8174a06771e358122a
user1 <user1@user1s-MacBook-Pro.local>
2017-12-12 10:27:24 +1100

    Added CryptoJS to dependenceies, simple setup of defining own key/data

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
+import CryptoJS from 'crypto-js'
+
+
+class App extends React.Component {
+  state = {
+    text: '',
+    key:''
+  }
   render() {
+    const { text, key } = this.state
+    var ciphertext = CryptoJS.AES.encrypt(text, key);
+
+    // Decrypt
+    var bytes = CryptoJS.AES.decrypt(ciphertext.toString(), key);
+    var plaintext = bytes.toString(CryptoJS.enc.Utf8);
+
+    console.log(plaintext);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+      Encrypt this data:
+        <input 
+        onChange={e => this.setState({ text: e.target.value }) } 
+        value={text} type="text" 
+        />
+      <br/>
+        Using this key: 
+        <input 
+        onChange={e => this.setState({ key: e.target.value }) } 
+        value={key} type="text" 
+        />
+        
+        Encrypted data: {ciphertext.toString()}
       </div>
     );
   }
 }
-
 export default App;
