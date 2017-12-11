551c87cbf095d1c39a4aa16a25cec481cb603344
user1 <user1@user1s-MacBook-Pro.local>
2017-12-12 10:41:18 +1100

    Now calculated how much bigger (percentage) it is to encrypt an item

--- src/App.js
+++ src/App.js
@@ -15,6 +15,10 @@ class App extends React.Component {
     var bytes = CryptoJS.AES.decrypt(ciphertext.toString(), key);
     var plaintext = bytes.toString(CryptoJS.enc.Utf8);
 
+    const textOrig = Buffer.byteLength(text, 'utf8')
+    const textEncr = Buffer.byteLength(ciphertext.toString(), 'utf8')
+    const percentageDif = (textEncr - textOrig ) * 10
+// 100 * |a - b| / ((a + b) * 2)
     console.log(plaintext);
     return (
       <div>
@@ -33,10 +37,13 @@ class App extends React.Component {
         Encrypted data raw value: {ciphertext.toString()}
         <br/>
         <b>
-        Original data size: {Buffer.byteLength(text, 'utf8')}
+        Original data size: {textOrig}
         <br/>
-        Encrypted data size: {Buffer.byteLength(ciphertext.toString(), 'utf8')}
+        Encrypted data size: {textEncr}
         </b>
+        <br/>
+        <br/>
+        <div> Encrypted is <b> {percentageDif}%</b> bigger than the plain string</div>
       </div>
     );
   }
